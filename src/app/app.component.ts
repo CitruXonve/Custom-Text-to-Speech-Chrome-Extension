25c011e5583a9e2375cfce5f65a3fe6d0be0e5ff
Angelika Shyu <angelikaxu54@gmail.com>
2021-09-29 17:45:27 -0700

    Implement basic Angular web UI logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gti-tts';
+  lang = 'en-US';
+
+  text_ts: string = 'Google Cloud Text-to-Speech enables developers to synthesize natural-sounding speech with 100+ voices, available in multiple languages and variants. It applies DeepMind’s groundbreaking research in WaveNet and Google’s powerful neural networks to deliver the highest fidelity possible. As an easy-to-use API, you can create lifelike interactions with your users, across many applications and devices.';
+
+  constructAPIRequest(text_ts: string, lang: string) {
+    return {
+      "audioConfig": {
+        "audioEncoding": "LINEAR16",
+        "pitch": 0,
+        "speakingRate": 1,
+      },
+      "input": {
+        "text": text_ts
+      },
+      "voice": {
+        "languageCode": lang,
+        "name": lang + "-Wavenet-A",
+        "ssmlGender": 'NEUTRAL'
+      }
+    };
+  }
+
+  public handleTextChange(text: string) {
+    this.text_ts = text;
+  }
+
+  public handleSpeakRequest(event: Event) {
+    event.preventDefault();
+
+    const requestBody = this.constructAPIRequest(this.text_ts, this.lang);
+    console.log(requestBody);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select'
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
